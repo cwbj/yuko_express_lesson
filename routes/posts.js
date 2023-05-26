4161f787c86f9667d5c8637552f88f2cf1593e7e
saitou0525 <[200~saitou@flowerbasket.jp~>
2023-05-26 16:34:21 +0900

    Add existing file

--- routes/posts.js
+++ routes/posts.js
@@ -27,8 +27,8 @@ router.get('/', function (req, res, next) {
 /**
  * create posts
  */
-router.get('/create',function(req,res,next){
-    res.render('posts/create',{
+router.get('/create', function (req, res, next) {
+    res.render('posts/create', {
         title: '',
         content: ''
     })
@@ -38,12 +38,12 @@ router.get('/create',function(req,res,next){
  * STORE POSTS
  */
 router.post('/store', function (req, res, next) {
-    
-    let title   = req.body.title;
-    let content = req.body.content;
-    let errors  = false;
 
-    if(title.length === 0) {
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
         errors = true;
 
         // set flash message
@@ -55,7 +55,7 @@ router.post('/store', function (req, res, next) {
         })
     }
 
-    if(content.length === 0) {
+    if (content.length === 0) {
         errors = true;
 
         // set flash message
@@ -68,25 +68,25 @@ router.post('/store', function (req, res, next) {
     }
 
     // if no error
-    if(!errors) {
+    if (!errors) {
 
         let formData = {
             title: title,
             content: content
         }
-        
+
         // insert query
-        connection.query('INSERT INTO posts SET ?', formData, function(err, result) {
+        connection.query('INSERT INTO posts SET ?', formData, function (err, result) {
             //if(err) throw err
             if (err) {
                 req.flash('error', err)
-                 
+
                 // render to add.ejs
                 res.render('posts/create', {
                     title: formData.title,
-                    content: formData.content                    
+                    content: formData.content
                 })
-            } else {                
+            } else {
                 req.flash('success', 'Data is successfully!');
                 res.redirect('/posts');
             }
@@ -98,35 +98,127 @@ router.post('/store', function (req, res, next) {
 /**
  * EDIT posts
  */
-router.get('/edit/(:id)',function(req,res,next) {
+router.get('/edit/(:id)', function (req, res, next) {
 
     let id = req.params.id;
 
-    connection.query('SELECT * FROM posts EHERE id = ' + id,function(err,rows,fields){
+    connection.query('SELECT * FROM posts WHERE id = ' + id, function (err, rows, fields) {
         // if there is an error on the query
-        if(err) throw err
+        if (err) throw err
 
-         // if user not found
-         if(rows.length <= 0){
-            req.flash('error','Post Data with ID ' + id + ' is not found')
+        // if user not found
+        if (rows.length <= 0) {
+            req.flash('error', 'Post Data with ID ' + id + ' is not found')
             res.redirect('/posts')
-         }
-         //if there is a data
-         else {
+        }
+        //if there is a data
+        else {
             res.render('posts/edit', {
                 id: rows[0].id,
                 title: rows[0].title,
                 content: rows[0].content
             })
-         }
-
-
+        }
     })
 })
 
+/**
+ * UPDATE POST
+ */
+
+router.post('/update/:id', function (req, res, next) {
+
+    let id = req.params.id;
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Title");
+        // render to edit.ejs with flash message
+        res.render('posts/edit', {
+            id: req.params.id,
+            title: title,
+            content: content
+        })
+    }
+
+    if (content.length === 0) {
+        errors = true;
+
+        // set flash message
+        req.flash('error', "Silahkan Masukkan Konten");
+        // render to edit.ejs with flash message
+        res.render('posts/edit', {
+            id: req.params.id,
+            title: title,
+            content: content
+        })
+    }
+
+    // if no error
+    if (!errors) {
+
+        let formData = {
+            title: title,
+            content: content
+        }
+
+        // update query
+        connection.query('UPDATE posts SET ? WHERE id = ' + id, formData, function (err, result) {
+            //if(err) throw err
+            if (err) {
+                // set flash message
+                req.flash('error', err)
+                // render to edit.ejs
+                res.render('posts/edit', {
+                    id: req.params.id,
+                    name: formData.name,
+                    author: formData.author
+                })
+            } else {
+                req.flash('success', 'Data Berhasil Diupdate!');
+                res.redirect('/posts');
+            }
+        })
+    }
+})
+
+/**
+ * Delete POST
+ */
+router.post('/delete/:id', function (req, res, next) {
+
+    let id = req.params.id;
+    let title = req.body.title;
+    let content = req.body.content;
+    let errors = false;
+
+    if (title.length === 0) {
+        errors = true;
 
 
+        // query
+        connection.query('DELETE FROM posts WHERE id = ' + id, function (err, result) {
+            if (err) {
+                // display message
+                req.flash('error', err)
+                // render
+                res.redirect('/posts', )
 
+            } else {
+                //display message
+                req.flash('success', 'The data is deleted!');
+                // render
+                res.redirect('/posts', );
 
+            }
+        })
+    }
+
+})
 
 module.exports = router;
